6ed4a91fec28bb2de6ec09635cd7dc727de62843
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-09-29 00:51:42 -0400

    Add round counter for games up to 5

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let computerChoice;      //Define variable for computer move choice
 let humanChoice;         //Define variable for human move choice
 
-let rock = "rock";      //Create variables naming each move as a string
-let paper = "paper";
-let scissor = "scissor";
+const rock = "rock";      //Create variables naming each move as a string
+const paper = "paper";
+const scissors = "scissors";
 
 /* Randomly generate computers move selection out of
 the 3 move types - rock, paper, and scissor. Generates a random number
@@ -16,43 +16,49 @@ function getComputerChoice() {
     } else if ((randomNumber > .33) && (randomNumber <  .66)) {
     return computerChoice = paper;
     } else { 
-    return computerChoice = scissor;
+    return computerChoice = scissors;
     }
 }
 
-getComputerChoice() //Generate computers move choice based on random move selection
-
-/* Prompt human player for their move selection
+/* Ask human player for their move selection
 Enter String of either R, P, or S */
+humanChoice = prompt(`Enter your move for Rock Paper Scissors, type in Rock, Paper, or Scissors`);
+console.log(typeof humanChoice);
+let adjustedHumanChoice = humanChoice.toLowerCase();
+console.log(adjustedHumanChoice);
 
-humanChoice = prompt(`Enter your move for Rock Paper Scissors, enter R for Rock, P for Paper, and S for Scissors`);
+getComputerChoice() //Generate computers move choice based on random move selection
+console.log(`Computer chose ${computerChoice}. You chose ${humanChoice}.`);
 
 let humanPoint = 0;  //Start both players with 0 point count
 let computerPoint = 0; 
-let startingRound = 0;  //Number setting starting round count
-let currentHumanScore  //Store number calculating current Human score
-let currentComputerScore  //Store number calculating current Computer score
+let currentHumanScore;  //Store number calculating current Human score
+let currentComputerScore;  //Store number calculating current Computer score
 
 /* Compare the move choices of both players to determine winner of the 
 current round. Rules for the outcome are that paper beats rock, rock beats
 scissors, and scissors beats paper */
 
-function winner(computerChoice, humanChoice) { 
+function playRound (humanChoice, computerChoice) { 
     if ((humanChoice === rock && computerChoice === rock)
         || (humanChoice === paper && computerChoice === paper)
-        || (humanChoice === scissor && computerChoice === scissor)) {
+        || (humanChoice === scissors && computerChoice === scissors)) {
                 console.log(`Tie result, better luck next round! The score is ${currentHumanScore} Human Player vs ${currentComputerScore} Computer Score`);
-        } else if ((humanChoice === rock && computerChoice === scissor)
+        } else if ((humanChoice === rock && computerChoice === scissors)
         || (humanChoice === paper && computerChoice === rock)
-        || (humanChoice === scissor && computerChoice === paper)) { 
+        || (humanChoice === scissors && computerChoice === paper)) { 
                 currentHumanScore = ++humanPoint;
                 console.log(`You beat the computer! The score is ${currentHumanScore} Human Player vs ${currentComputerScore} Computer Score`);
        } else if ((humanChoice === rock && computerChoice === paper)
-        || (humanChoice === paper && computerChoice === scissor)
-        || (humanChoice === scissor && computerChoice === rock)) {
+        || (humanChoice === paper && computerChoice === scissors)
+        || (humanChoice === scissors && computerChoice === rock)) {
                 currentComputerScore = ++computerPoint;
                 console.log(`You LOSE this round! The score is ${currentHumanScore} Human Player vs ${currentComputerScore} Computer Score`);
 }
 }
 
-let currentRound = startingRound + humanPoint + computerPoint //Define variable for calculating current game round number 
+function playGame() {
+for (let currentRound = 0; currentRound <= 5; currentRound++) {  //Create round counter for duration of 5 total round game
+    if roundWinner()
+}
+}
