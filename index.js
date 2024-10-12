c5c703549c2298320c58fd7c4ef792096fd1cda4
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-11 23:25:24 -0400

    Update PlayRound feature to include 5 rounds, show and count points of human vs computer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let computerChoice;      //Define variable to store computer move choice
-let humanChoice
+let humanChoice;
+let computerPoint = 0;
+let humanPoint = 0;
 
 const rock = "rock";      //Create variables naming each move as a string
 const paper = "paper";
@@ -11,6 +13,13 @@ const paperChoice = document.querySelector('.paper.btn')
 const scissorsChoice = document.querySelector('.scissor.btn')
 const results = document.querySelector('.results')
 
+const scoreCount = document.createElement("div")
+scoreCount.classList.toggle("scoreCount");
+scoreCount.textContent = `Your score is ` + humanPoint + ` and the computer score is ` + computerPoint + `!`;
+results.append(scoreCount);
+
+
+
 container.addEventListener("click", (move) => {
     let target = move.target;
 
@@ -18,19 +27,16 @@ container.addEventListener("click", (move) => {
         case 'rock':
             humanChoice = rock;
             console.log(humanChoice)
-            showMoveChoices()
             playRound()
        break;
         case 'paper':
             humanChoice = paper;
             console.log(humanChoice)
-            showMoveChoices()
             playRound()
         break;
         case 'scissors':
             humanChoice = scissors;
             console.log(humanChoice)
-            showMoveChoices()
             playRound()
         break;
     }
@@ -51,10 +57,8 @@ function getComputerChoice() {
     }
 }
 
-function showMoveChoices(){
-const moveChoices = document.createElement("p")
-moveChoices.textContent = `Computer chose ${computerChoice}. You chose ${humanChoice}.`
-results.appendChild(moveChoices)
+function showMoves() {
+    theMoves.textContent = `Your move is ${humanChoice} and the computers move is ${computerChoice}`;
 }
 
 /* Compare the move choices of both players to determine winner of the 
@@ -63,29 +67,38 @@ scissors, and scissors beats paper */
 
 function showWinner(humanChoice, computerChoice) { 
     if (humanChoice === computerChoice) {
-            console.log(`Tie result, better luck next round! The score is ${currentHumanScore} Human Player vs ${currentComputerScore} Computer Score`);
+        scoreCount.textContent = `Tie result, better luck next round! The score is ${humanPoint} Human Player vs ${computerPoint} Computer Score`;
         } else if ((humanChoice === rock && computerChoice === scissors)
         || (humanChoice === paper && computerChoice === rock)
         || (humanChoice === scissors && computerChoice === paper)) { 
-                currentHumanScore = ++humanPoint;
-                console.log(`You beat the computer! The score is ${currentHumanScore} Human Player vs ${currentComputerScore} Computer Score`);
+                humanPoint = ++humanPoint;
+                scoreCount.textContent = `You beat the computer! The score is ${humanPoint} Human Player vs ${computerPoint} Computer Score`;
        } else if ((humanChoice === rock && computerChoice === paper)
         || (humanChoice === paper && computerChoice === scissors)
         || (humanChoice === scissors && computerChoice === rock)) {
-                currentComputerScore = ++computerPoint;
-                console.log(`You LOSE this round! The score is ${currentHumanScore} Human Player vs ${currentComputerScore} Computer Score`);
+                computerPoint = ++computerPoint;
+                scoreCount.textContent = `You LOSE this round! The score is ${humanPoint} Human Player vs ${computerPoint} Computer Score`;
 }
 }
 
 
 function playRound() {     //Create function to call start a round
     getComputerChoice()
-    displayMoveChoices()
+    showMoves()
     showWinner(humanChoice, computerChoice)
+    for (let i = 0; i < 5; i++){
+        let totalPoints = computerPoint + humanPoint;
+        if (totalPoints === 5){
+            humanPoint = 0;
+            computerPoint = 0;
+            i = 0;
+        }
+    }
 }
 
+const theMoves = document.createElement("p")
+theMoves.classList.toggle("theMoves")
+results.append(theMoves)
 
-function playGame () {                                               
-    playRound()
-    } 
+
 
